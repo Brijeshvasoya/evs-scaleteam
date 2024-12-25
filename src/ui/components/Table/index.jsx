58928b6336859e907f9ee005e3a3c87459428058
Brijeshvasoya <brijeshvasoya@gmail.com>
2024-12-25 17:54:51 +0530

    Make table ui

--- src/ui/components/Table/index.jsx
+++ src/ui/components/Table/index.jsx
@@ -1,19 +1,50 @@
 import React, { Fragment } from "react";
 import DataTable from "react-data-table-component";
-import { ChevronDown } from "react-feather";
-import _ from 'lodash';
+import { AlignCenter, ChevronDown } from "react-feather";
+import _ from "lodash";
 import "./table.css";
 
 const Table = ({ columns, data, editData, deleteData }) => {
-    console.log(data,"data")
+  const customStyles = {
+    rows: {
+      style: {
+        border: "1px solid #f3f2f7",
+        backgroundColor: "transparent",
+      },
+    },
+    headCells: {
+      style: {
+        minHeight: "40px",
+        backgroundColor: '#f3f2f7'
+      },
+    },
+    table: {
+      style: {
+        padding: "1rem 0" ,
+        backgroundColor: "transparent",
+      },
+    },
+    cells: {
+      style: {
+        textTransform: "capitalize",
+        backgroundColor:"#f3f2f0"
+      },
+    },
+  };
   return (
     <Fragment>
       <div className="react-dataTable" id="data-table">
         <DataTable
           className="react-dataTable"
-          columns={_.filter(columns, (column) => column !== undefined && column !== null)}  
-          sortIcon={<ChevronDown size={10} />} 
-          data={data?.filter((item) => item !== undefined && item !== null) || []}  
+          columns={_.filter(
+            columns,
+            (column) => column !== undefined && column !== null
+          )}
+          sortIcon={<ChevronDown size={10} />}
+          data={
+            data?.filter((item) => item !== undefined && item !== null) || []
+          }
+          customStyles={customStyles}
         />
       </div>
     </Fragment>
